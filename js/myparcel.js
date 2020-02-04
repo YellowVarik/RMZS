046ba18e947d8a42cc86dd4261942dc2c6edb0cb
Youri <yourigooyer@gmail.com>
2020-02-04 13:55:00 +0100

    Documentatie toegevoegd

--- js/myparcel.js
+++ js/myparcel.js
@@ -1,19 +1,27 @@
 const https = require('https')
 const fs = require('fs')
+
+//jQuery moet op een andere manier worden toegevoegd
 window.$ = window.jQuery = require('./js/jquery-3.4.1.min.js')
 
+
 var mpURL = "api.myparcel.nl";
+
+//De key moet worden opgrvraagd door de gebruiker in hun MyParcel account
 var mpKey = "914bb634d3cf4a01ba809dd4b121e33f9d2ea50a";
 
 getMyParcelData();
 
 function getMyParcelData(){
+  //De key moet met base64 worden versleuteld
   let keyBuffer = new Buffer.from(mpKey);
   let base64Key = keyBuffer.toString("base64");
 
   
   var data = "";
 
+
+  //Deze opties zijn standaard voor de API en komen van de documentatie op https://myparcelnl.github.io/api
   var options = {
     hostname: mpURL,
     path: "/shipments",
@@ -33,19 +41,23 @@ function getMyParcelData(){
 
   var request = https.request(options, function(result){
     result.on('data', (d) => {
+      //De tussentijdse data wordt toegevoegd aan een variabele
       data += d;
     });
     result.on("end", () => {
+        //De data wordt verwerkt
         let parsedData = JSON.parse(data);
         let count = parsedData.data.results;
-        console.log(count);
         let zending = parsedData.data.shipments;
+
+        //Elke zending wordt apart weergegeven
         for(var i = 0; i < count; i++){
           let name = "<p>Naam: " + zending[i].recipient.person + "</p>";
           let adress = "<p>Adres: " + zending[i].recipient.street + " " + zending[i].recipient.number + " " + zending[i].recipient.city + "</p>";
           $("#myparcel").append(name, adress);
         }
         
+        //De data wordt opgeslagen in een bestand
         fs.writeFile("data/zendingen.json", data, (e) => {
             if(e) throw e;
             console.log("Data opgeslagen!");
@@ -55,6 +67,7 @@ function getMyParcelData(){
   request.on('error', (e) => {
     console.error("KON DATA VAN MYPARCEL NIET OPHALEN \n" + e);
 
+    //Als er geen data kon worden opgehaald wordt het uit een bestand gehaald
     if(fs.existsSync("./data/zendingen.json")){
       fs.readFile("data/zendingen.json", function(err, data){
         let parsedData = JSON.parse(data);
