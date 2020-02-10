808d0a3ddc2245237fffc30f2a75f78d8f7b67b2
patrick <phvanschaik@hotmail.nl>
2020-02-10 14:00:00 +0100

    icon

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function createWindow () {
     width: 800,
     height: 600,
     autoHideMenuBar: true,
-    icon: './img/logo.png',
+    icon: './img/icon.png',
     webPreferences: {
       nodeIntegration: true
     }
