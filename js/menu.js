93ba331030d812da27992dc090b695d5c6e07661
patrick <phvanschaik@hotmail.nl>
2020-03-09 14:55:09 +0100

    menuutje

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,16 @@
 //jQuery moet op een andere manier worden toegevoegd
 window.$ = window.jQuery = require('./js/jquery-3.4.1.min.js')
+var extended = false;
 
-$(document).ready(function () {
+function extendMenu(){
+    if(extended){
+        $('.sidebar').eq(0).removeClass("extended")
+        extended = false;
+    }
+    else{
+        $('.sidebar').eq(0).addClass("extended")
+        extended = true;
+    }
+}
 
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-    });
 
-});
