0ae8e4eac81cfe334a5362cd8fe79d97d35f3ff9
Yellow Varik <hugovanschalm@hotmail.com>
2020-03-20 15:39:28 +0100

    login

--- js/account.js
+++ js/account.js
@@ -83,15 +83,15 @@ function login(){
 
     if(username != configData.username){
         error = true;
-        errormsg += "Gebruikersnaam komt niet overeen!<br><br>";
+        errormsg = "Gebruikersnaam of wachtwoord komt niet overeen!<br><br>";
     }
     if(password != configData.password){
         error = true;
-        errormsg += "Wachtwoord komt niet overeen!<br><br>";
+        errormsg = "Gebruikersnaam of wachtwoord komt niet overeen!<br><br>";
     }
 
     if(error){
-        document.getElementsByClassName('errormsg')[0].innerHTML = errormsg;
+        document.getElementsByClassName('errormsg')[0].innerHTML = errormsg + "<font style='font-size: 10px;'><b>Klopt dit niet? Neem contact op met de systeembeheerder!</b></font>";
         document.getElementById("popup1").classList.add("visible")
         return
     }
