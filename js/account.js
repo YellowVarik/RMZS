c898b7aac8793848e67a05ca4c8a12f1f98ff0cc
Yellow Varik <hugovanschalm@hotmail.com>
2020-03-20 15:10:52 +0100

    account

--- js/account.js
+++ js/account.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-if(!fs.existsSync('/../config/config.json') && window.location.pathname.split('/').pop() !== 'keys.html'){
+if(!fs.existsSync('./config/config.json') && window.location.pathname.split('/').pop() !== 'keys.html'){
     window.location.href = "./keys.html";
 }
 
@@ -13,35 +13,43 @@ function makeAccount(){
     let lsSecret = document.getElementById('lsSecretInput').value;
 
     var error = false;
-    var errormsg = "";
+    var errormsg = "<br>";
 
     if(name.length < 1){
-        errormsg += "De naam moet minstens 1 teken lang zijn<br>"
+        errormsg += "De naam moet minstens 1 teken lang zijn<br><br>"
+        document.getElementById('naamInput').value = "";
         error = true;
     }
     if(uname.length < 1){
-        errormsg += "De gebruikersnaam moet minstens 1 teken lang zijn<br>"
+        errormsg += "De gebruikersnaam moet minstens 1 teken lang zijn<br><br>"
+        document.getElementById('usernameInput').value = "";
         error = true;
     }
     if(password.length < 1){
-        errormsg += "Het wachtwoord moet minstens 1 teken lang zijn<br>"
+        errormsg += "Het wachtwoord moet minstens 1 teken lang zijn<br><br>"
+        document.getElementById('passwordInput').value = "";
         error = true;
     }
     if(mpKey.length < 40){
-        errormsg += "De MyParcel API key moet minstens 40 tekens lang zijn<br>"
+        errormsg += "De MyParcel API key moet minstens 40 tekens lang zijn<br><br>"
+        document.getElementById('mpKeyInput').value = "";
         error = true;
     }
     if(lsKey.length < 32){
-        errormsg += "De Lightspeed API key moet minstens 32 teken lang zijn<br>"
+        errormsg += "De Lightspeed API key moet minstens 32 teken lang zijn<br><br>"
+        document.getElementById('lsKeyInput').value = "";
         error = true;
     }
     if(lsSecret.length < 32){
-        errormsg += "De Lightspeed API Secret moet minstens 32 teken lang zijn<br>"
+        errormsg += "De Lightspeed API Secret moet minstens 32 teken lang zijn<br><br>"
+        document.getElementById('lsSecretInput').value = "";
         error = true;
     }
 
     if(error){
-
+        document.getElementsByClassName('errormsg')[0].innerHTML = errormsg;
+        document.getElementById("popup1").classList.add("visible")
+        return
     }
     
 
@@ -53,4 +61,32 @@ function makeAccount(){
     } catch(error){
         console.error(error);
     }
+}
+
+function login(){
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    configFile = fs.readFileSync('./config/config.json');
+    configData = JSON.parse(configFile);
+
+    let error = false;
+    let errormsg = '<br>';
+
+    if(username != configData.username){
+        error = true;
+        errormsg += "Gebruikersnaam komt niet overeen!<br>";
+    }
+    if(password != configData.password){
+        error = true;
+        errormsg += "Wachtwoord komt niet overeen!<br>";
+    }
+
+    if(error){
+        document.getElementsByClassName('errormsg')[0].innerHTML = errormsg;
+        document.getElementById("popup1").classList.add("visible")
+        return
+    }
+
+    window.location.href = './home.html';
 }
