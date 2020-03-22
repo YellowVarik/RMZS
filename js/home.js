e46a98f6141456e6de48e861a448c1070d850bab
Yellow Varik <hugovanschalm@hotmail.com>
2020-03-22 17:05:55 +0100

    laatste week

--- js/home.js
+++ js/home.js
@@ -12,10 +12,14 @@ async function getSales(){
         console.log(result.data);
         var thisyear = new Date().getFullYear();
         var dashboard = result.data.dashboard;
+        var week = 0;
         var thirtydays = 0;
         var ninetydays = 0;
         var year = 0;
         var alltime = dashboard.totals.salesIncl;
+        for(var i = 0; i < dashboard.periods.length && i < 7; i++){
+            week += dashboard.periods[i].salesIncl;
+        }
         for(var i = 0; i < dashboard.periods.length && i < 30; i++){
             thirtydays += dashboard.periods[i].salesIncl;
         }
@@ -26,6 +30,7 @@ async function getSales(){
             year += dashboard.periods[i].salesIncl;
         }
         
+        document.getElementById('week').innerHTML = "<i class='fas fa-coins'></i> Inkomsten laatste 7 dagen: \u20AC" + week;
         document.getElementById('30days').innerHTML = "<i class='fas fa-coins'></i> Inkomsten laatste 30 dagen: \u20AC" + thirtydays;
         document.getElementById('90days').innerHTML = "<i class='fas fa-coins'></i> Inkomsten laatste 90 dagen: \u20AC" + ninetydays;
         document.getElementById('year').innerHTML = "<i class='fas fa-coins'></i> Inkomsten dit jaar: \u20AC" + year;
