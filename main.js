584890641c7f0f3503352327cf13a65c043c8246
Yellow Varik <hugovanschalm@hotmail.com>
2020-03-25 20:24:22 +0100

    autoupdate

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, autoUpdater, dialog } = require('electron')
+const log = require('electron-log')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
+
 let win
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
     width: 800,
@@ -22,7 +24,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   win.loadFile('index.html')
-  
+
 
   // Emitted when the window is closed.
   win.on('closed', () => {
@@ -40,7 +42,9 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  createWindow();
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -62,3 +66,7 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+require('update-electron-app')({
+  log: log.log
+})
