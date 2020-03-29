74fd75ed9bde08c078151707c013ab6d430b2e39
Yellow Varik <hugovanschalm@hotmail.com>
2020-03-29 15:21:11 +0200

    Checken of titel lang genoeg is

--- js/settings.js
+++ js/settings.js
@@ -14,14 +14,19 @@ async function showStatusses() {
     var addButton = $('<button class="addStatus">Status toevoegen</button>').appendTo('#customStatusSettings');
     addButton.click(() => {
         let popup = $('#customStatusPopup');
-        console.log('bruh');
+        let title = popup.find('.title').val('');
+        popup.find('.errormsg').css('visibility', 'hidden');
         popup.find('h2').eq(0).text('Status toevoegen');
         popup.find('.colorPicker').eq(0).empty();
         colorPicker.from(popup.find('.colorPicker').eq(0));
-        popup.find('.save').off('click').click(async function(){
+        popup.find('.save').off('click').click(async function () {
             let color = popup.find('.a-color-picker-rgbhex').eq(0).find('input').eq(0).val();
             let title = popup.find('.title').val();
-            console.log({color, title});
+            if (title.length < 1) {
+                popup.find('.errormsg').css('visibility', 'visible');
+                return
+            }
+            console.log({ color, title });
             let customStatus = await lightspeed.addCustomStatus(color, title);
             console.log(customStatus)
             addStatus(customStatus);
@@ -32,28 +37,33 @@ async function showStatusses() {
     })
 }
 
-async function addStatus(customStatus){
+async function addStatus(customStatus) {
     let div = $(`<div class='customStatus'><span><h3 style='color:${customStatus.color}'>${customStatus.title}</h3><a class='delete'><i class='fas fa-trash'></i></a><a class='edit'><i class='fas fa-edit'></i></a></span></div></div>`).appendTo('#customStatusSettings');
-        div.find('.edit').eq(0).click(() => {
-            let popup = $('#customStatusPopup');
-            popup.find('h2').eq(0).text('Status aanpassen');
-            popup.find('.title').eq(0).val(customStatus.title);
-            popup.find('.colorPicker').eq(0).empty().attr('acp-color', customStatus.color);
-            colorPicker.from(popup.find('.colorPicker').eq(0));
+    div.find('.edit').eq(0).click(() => {
+        let popup = $('#customStatusPopup');
+        popup.find('.errormsg').css('visibility', 'hidden');
+        popup.find('h2').eq(0).text('Status aanpassen');
+        popup.find('.title').eq(0).val(customStatus.title);
+        popup.find('.colorPicker').eq(0).empty().attr('acp-color', customStatus.color);
+        colorPicker.from(popup.find('.colorPicker').eq(0));
 
-            popup.find('.save').off('click').click(()=>{
-                let color = popup.find('.a-color-picker-rgbhex').eq(0).find('input').eq(0).val();
-                let title = popup.find('.title').val();
-                if(title === customStatus.title && color == customStatus.color){
-                    popup.removeClass('visible')
-                }else{
-                    id = customStatus.id;
-                    lightspeed.updateCustomStatus(color, title, id);
-                    div.find('h3').eq(0).text(title).css('color', color);
-                    popup.removeClass('visible')
-                }
-            });
+        popup.find('.save').off('click').click(() => {
+            let color = popup.find('.a-color-picker-rgbhex').eq(0).find('input').eq(0).val();
+            let title = popup.find('.title').val();
+            if (title.length < 1) {
+                popup.find('.errormsg').css('visibility', 'visible');
+                return
+            }
+            if (title === customStatus.title && color == customStatus.color) {
+                popup.removeClass('visible')
+            } else {
+                id = customStatus.id;
+                lightspeed.updateCustomStatus(color, title, id);
+                div.find('h3').eq(0).text(title).css('color', color);
+                popup.removeClass('visible')
+            }
+        });
 
-            popup.addClass('visible');
-        })
+        popup.addClass('visible');
+    })
 }
