2a5d8d66b205d44c3a11641ddb0363511d61d274
Yellow Varik <hugovanschalm@hotmail.com>
2020-04-14 20:24:32 +0200

    testt

--- main.js
+++ main.js
@@ -67,7 +67,3 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
-require('update-electron-app')({
-  log: log.log
-})
